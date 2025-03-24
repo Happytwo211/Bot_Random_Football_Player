ab6b2339bfa7256388ad2f56e8ac1e999effc326
DanielDitrikh <daniel.ditrikh@mail.ru>
2025-03-25 02:19:02 +0300

    initial

--- main.py
+++ main.py
@@ -26,28 +26,36 @@ player_name = {
 @bot.message_handler(commands=['start'])
 def register_start(message):
     result = random_choise(player_name)
-    bot.send_message(message.chat.id, f'{message.from_user.first_name}, {result}', reply_markup=show_random_choice_keyboard())
+    bot.send_message(message.chat.id, f'{message.from_user.first_name}, {result}', parse_mode='HTML')
+                        # reply_markup=show_random_choice_keyboard(),
 
-
-@bot.callback_query_handler(func=lambda call: call.data == 'random_choice')
-def callback_handler(call):
-    result = random_choise(player_name)
-    bot.edit_message_text(
-        text=f'{result}',
-        chat_id=call.message.chat.id,
-        message_id=call.message.message_id,
-        reply_markup=show_random_choice_keyboard()
-    )
+# @bot.callback_query_handler(func=lambda call: call.data == 'random_choice')
+# def callback_handler(call):
+#     result = random_choise(player_name)
+#     bot.edit_message_text(
+#         text=f'{result}',
+#         chat_id=call.message.chat.id,
+#         message_id=call.message.message_id,
+#         reply_markup=show_random_choice_keyboard(),
+#         parse_mode='HTML'
+#     )
 
 @bot.message_handler(content_types=['text'])
 def handle_group_messages(message):
-    # if message.chat.type == 'group':
-    #     bot.send_message(message.chat.id, 'Приветствую вас в групповом чате!')
     if message.text == 'кто я':
         result = random_choise(player_name)
-        bot.send_message(message.chat.id, f'{message.from_user.first_name}, {result}', reply_markup=show_random_choice_keyboard())
+        bot.send_message(message.chat.id, f'{message.from_user.first_name}, {result}',)
+                         # reply_markup=show_random_choice_keyboard())
 
 
+# @bot.message_handler(content_types=['text'], func=lambda m: '@' + bot.get_me().username in m.text)
+# def handle_mentions(message):
+#
+#     if 'кто я' in message.text.lower():
+#         result = random_choise(player_name)
+#         bot.reply_to(message, f'{message.from_user.first_name}, {result}', reply_markup=show_random_choice_keyboard())
+#     else:
+#         bot.reply_to(message, 'Я здесь, чем могу помочь?')
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
